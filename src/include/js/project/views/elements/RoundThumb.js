88945bff61da9b06c0bed2bb46606d64cb4406d5
yixiongz <yixiong.zhou@b-reel.com>
2013-01-22 17:59:19 -0500

    image zoom out effect for roundthumb.js

--- src/include/js/project/views/elements/RoundThumb.js
+++ src/include/js/project/views/elements/RoundThumb.js
@@ -11,7 +11,7 @@ goog.require('goog.style');
 joeonmars.views.elements.RoundThumb = function(radius, position) {
   goog.base(this);
 
-  this.tweenRadiusObj = {outerRad:0, innerRad:0};
+  this.tweenRadiusObj = {outerRad:0, innerRad:0, backgroundSize:0};
 
   // set thumbnail's default size
   this.radius = radius || joeonmars.views.elements.RoundThumb.Radius.LARGE;
@@ -57,21 +57,21 @@ joeonmars.views.elements.RoundThumb.prototype.deactivate = function() {
 joeonmars.views.elements.RoundThumb.prototype.setStatus = function(status, isInstant) {
   switch(status) {
     case joeonmars.views.elements.RoundThumb.Status.DEFAULT:
-      this.tweenRadiusTo(this.radius, this.radius * (1 - joeonmars.views.elements.RoundThumb.BorderRatio.DEFAULT), isInstant);
+      this.tweenTo(this.radius, this.radius * (1 - joeonmars.views.elements.RoundThumb.BorderRatio.DEFAULT), 100, isInstant);
       break;
 
     case joeonmars.views.elements.RoundThumb.Status.EXPANDED:
-      this.tweenRadiusTo(this.radius + 20, (this.radius + 20) * (1 - joeonmars.views.elements.RoundThumb.BorderRatio.EXPANDED));
+      this.tweenTo(this.radius + 20, (this.radius + 20) * (1 - joeonmars.views.elements.RoundThumb.BorderRatio.EXPANDED), 80);
       break;
 
     case joeonmars.views.elements.RoundThumb.Status.SHRINKED:
-      this.tweenRadiusTo(this.radius * .5, 0);
+      this.tweenTo(this.radius * .5, 0);
       break;
   }
 };
 
 
-joeonmars.views.elements.RoundThumb.prototype.tweenRadiusTo = function(toOuterRad, toInnerRad, isInstant) {
+joeonmars.views.elements.RoundThumb.prototype.tweenTo = function(toOuterRad, toInnerRad, backgroundSize, isInstant) {
   TweenMax.killTweensOf(this.tweenRadiusObj);
 
   var duration = (isInstant === true) ? 0 : .8;
@@ -79,6 +79,7 @@ joeonmars.views.elements.RoundThumb.prototype.tweenRadiusTo = function(toOuterRa
   TweenMax.to(this.tweenRadiusObj, duration, {
     outerRad: toOuterRad,
     innerRad: toInnerRad,
+    backgroundSize: backgroundSize || this.tweenRadiusObj.backgroundSize,
     'ease': Strong.easeOut,
     'onUpdate': function() {
       var currentOuterRad = this.tweenRadiusObj.outerRad;
@@ -90,7 +91,7 @@ joeonmars.views.elements.RoundThumb.prototype.tweenRadiusTo = function(toOuterRa
       var innerPos = - currentInnerRad;
 
       goog.style.setStyle(this.outerDom, {'width': outerDia + 'px', 'height': outerDia + 'px', 'left': outerPos + 'px', 'top': outerPos + 'px'});
-      goog.style.setStyle(this.innerDom, {'width': innerDia + 'px', 'height': innerDia + 'px', 'left': innerPos + 'px', 'top': innerPos + 'px'});
+      goog.style.setStyle(this.innerDom, {'width': innerDia + 'px', 'height': innerDia + 'px', 'left': innerPos + 'px', 'top': innerPos + 'px', 'background-size': this.tweenRadiusObj.backgroundSize + '%'});
     },
     'onUpdateScope': this
   });
@@ -119,7 +120,7 @@ joeonmars.views.elements.RoundThumb.prototype.onResize = function(windowSize) {
 
 joeonmars.views.elements.RoundThumb.BorderRatio = {
   DEFAULT: .06,
-  EXPANDED: .03
+  EXPANDED: .02
 };
 
 
